4a100d64503df87abaef044982bcd48865bb6b59
SohiniBoral16 <boralsohini@gmail.com>
2023-06-06 11:59:22 +0530

    first commit

---
+++ src/test/java/com/intuit/crowdfundingServiceRegistry/CrowdfundingServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intuit.crowdfundingServiceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowdfundingServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
